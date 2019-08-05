3343ac14daf07af071f6c43468b1a26bb361a55d
Akiliki <akiliki@gmail.com>
2019-08-06 00:18:28 +0200

    Añadimos uso de librerias externas

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+console.log("Si traemos un proyecto desde GIT tenemos que hacer npm install para descargarnos las dependecias")
+
+
 const math = require("./math")
 const greet = require("./grettings")
 const hello = require("./grettings/hello")
+const cowsay = require("cowsay")
 
 console.log(math.add(1, 122121))
 console.log(math.divide(1, 122121))
@@ -11,3 +15,10 @@ console.log(math.multiply(1, 122121))
 console.log(greet.greet("Aitor"))
 
 console.log(hello.sayHello("Aitor"))
+
+console.log(cowsay.say({
+                    text: "Kaixo Aitor",
+                    e: "o0",
+                    T: "A"
+                })
+            )
