b8e3cbfc66ef4f27cc7d6ad75307ce1019d2d09b
Akiliki <akiliki@gmail.com>
2019-08-06 00:07:55 +0200

    Añadimos ejemplos de uso de modulos locales

---
+++ index.js
@@ -0,0 +1,13 @@
+const math = require("./math")
+const greet = require("./grettings")
+const hello = require("./grettings/hello")
+
+console.log(math.add(1, 122121))
+console.log(math.divide(1, 122121))
+console.log(math.substract(1, 122121))
+console.log(math.multiply(1, 122121))
+
+
+console.log(greet.greet("Aitor"))
+
+console.log(hello.sayHello("Aitor"))
