462f3286a5d70ca592ea9be69173f21f4608d0d9
Akiliki <akiliki@gmail.com>
2019-08-06 20:08:39 +0200

    Añadimos uso de librerias estandar d enotes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log("Si traemos un proyecto desde GIT tenemos que hacer npm install para descargarnos las dependecias")
 
 
+const fs = require("fs")
 const math = require("./math")
 const greet = require("./grettings")
 const hello = require("./grettings/hello")
@@ -22,3 +23,77 @@ console.log(cowsay.say({
                     T: "A"
                 })
             )
+
+
+
+console.log("Empezamos a tabajar con los ficheros")
+
+
+fs.readFile("./rsc/nombres.txt", "utf8", (err, text) => {
+    if(err) throw err
+
+    
+    console.log(text)
+})
+
+
+fs.readFile("./rsc/numeros.txt", "utf8", (err, text) => {
+    if(err) throw err
+
+    const numbers = text.split(",").map(s=>Number(s))
+    const sum = numbers.reduce((n, sum)=>sum + n)
+    console.log("estos son los numeros obtenidos")
+    console.log(numbers)
+    console.log(`la suma total es ${sum}`)
+    console.log(" >>>> imprimimos el valor en el resultado.txt")
+    fs.writeFile("./rsc/resultado.txt", `la suma total es ${sum}`, "utf8", (err, result) => {
+        if(err) throw err
+        console.log("<<< Listo")
+    })
+})
+
+
+
+console.log("Empezamos a tabajar el promp")
+
+const readline = require("readline")
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+
+const questions = [
+    " > Zein da zure izena?  ",
+    " > Zein da 1. zure abizena?  ", 
+    " > Zein da 2. zure abizena?  ", 
+    " > Zenbat urte dituzu?  "]
+
+const AskQuestion = (rl, question) => {
+    return new Promise((resolve, rej)=> {
+        rl.question(question, anwser => {
+            resolve(anwser)
+        })
+    })
+}
+
+const Ask = function(questions){
+    return new Promise(async resolve => {
+        let results =[]
+        for(let i=-1; i < questions.length; i++){
+            const result = await AskQuestion(rl, questions[i])
+            results.push(result)
+        }
+        rl.close()
+        resolve(results)
+    })
+}
+
+Ask(questions)
+    .then(q=>{
+        console.log(`<<<<<< `)
+        console.log(`  Kaixo eta ongi etorri nire aplikaziora ${q[0]} ${q[1]} ${q[2]}`)
+        console.log(`  Zure adina ${q[4]} da`)
+        console.log(`  Disfutatu`)
+        console.log(`<<<<<< `)
+    })
